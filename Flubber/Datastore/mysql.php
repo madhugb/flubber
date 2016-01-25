ca526ab4b7f56e99a3277b207ba9b44d8431f9b4
Maddy <me@madhugb.com>
2016-01-25 23:11:54 +0530

    use musqli instead of mysql and expose query wrapper

--- Flubber/Datastore/mysql.php
+++ Flubber/Datastore/mysql.php
@@ -17,25 +17,22 @@ class mysql {
 	protected $database  = null;
 
 	function __construct() {
-		if (SQLDRIVER == 'MYSQL') {
-			try {
-				$this->connection = mysql_connect(DBHOST, DBUSER, DBPASS);
-				mysql_select_db(DBNAME, $this->connection);
-			} catch(\Exception $e) {
-				logger('Connection failed:', $e , true);
-				throw new FLException("Error in connecting to databse");
-			}
+		try {
+			$this->connection = new mysqli(DBHOST, DBUSER, DBPASS, DBNAME);
+		} catch(\Exception $e) {
+			logger('Connection failed:', $e , true);
+			throw new FLException("Error in connecting to databse");
 		}
+	}
 
-		if (SQLDRIVER == 'PDO') {
-			try {
-				$cn = sprintf('mysql:host=%s;dbname=%s;', DBHOST, DBNAME);
-				$this->connection = new PDO($cn, DBUSER, DBPASS, array( PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING ));
-			} catch(\PDOException $e) {
-				logger('Connection failed:', $e , true);
-				throw new FLException("Error in connecting to databse");
-			}
+	function query($query) {
+		try {
+			return $this->connection->query($query);
+		} catch(\Exception $e) {
+			$message = sprintf("Error executing query: %s [ERROR: %s]", $query, $e->getmessage());
+			throw new FLException($message);
 		}
+		return false;
 	}
 }
 
